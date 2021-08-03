0f150e8af08e4525c5ea0b2d60d2033b567439b8
claudiahW <murungiclaud@gmail.com>
2021-08-03 11:30:31 +0300

    allow hovering effet on icons and disappearing text

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.design').click(function() {
+        $('#design').show();
+        $(".design").hide();
+
+    }); $('#design').click(function() {
+        $('.design').show();
+        $("#design").hide();
+
+    });
+    $('.development').click(function() {
+        $('#development').show();
+        $(".development").hide();
+    });
